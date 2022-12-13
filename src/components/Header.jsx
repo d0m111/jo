cd1bc9de212cf2c8fa6aed22ba0c4a3e93859fe1
francisco <franciscouberto@gmail.com>
2022-12-14 00:34:05 +0100

    implementing the nav bar (incomplete)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,14 @@ export default function Header() {
      return (
           <div>
                <h1>JOAQUIN ORTEGA</h1>
-               
+               <nav>
+                    <ul>
+                         <li>MUSIC</li>
+                         <li>solo</li>
+                         <li>Blast</li>
+                         <li>La Grote</li>
+                    </ul>
+               </nav>
           </div>
      );
 }
