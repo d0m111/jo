3f509989a56c90fc62f1d9bfd28d5bf5a3b69c72
francisco <franciscouberto@gmail.com>
2023-01-12 16:30:02 +0100

    other minor details here and there (i forgot)

--- src/components/Blast.jsx
+++ src/components/Blast.jsx
@@ -11,8 +11,7 @@ export default function Blast(){
             </div>
 
             <div className="solo-content-wrapper">
-            <p>The solo invites us to immerse ourselves in a deep listening, constantly nourishing the search for an interaction of sound objects in space.
-                Tremors, resonances or vibrations create a world in perpetual mutation, in a game of discontinuity/continuity. </p>
+            <p>We just play, spontaneously, with no prior talking, plans, and all that crap.</p>
                 <div className="iframe-wrapper">
                     <iframe className="solo-yt-iframe" src="https://www.youtube.com/embed/pWni0wybGSw?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     <iframe  className="bandCamp-iframe" title="band camp player" src="https://bandcamp.com/EmbeddedPlayer/album=1371569582/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/" seamless><a href="https://joaquinortega.bandcamp.com/album/post-post-post-era">post-post-post-era by Francisco Uberto, Joaquin Ortega</a></iframe>
